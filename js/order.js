cc4674fd23a2b97cd0b7904cc3b3c8287cc6f8ab
gunbatimi-cafe <kocdogukan98@gmail.com>
2026-07-07 13:07:06 +0300

    Create order.js

---
+++ js/order.js
@@ -0,0 +1,164 @@
+import { getProducts } from "./firestore.js";
+import { db } from "./firebase.js";
+
+import {
+collection,
+addDoc,
+serverTimestamp
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+
+let products = [];
+let cart = [];
+
+
+const productsDiv = document.getElementById("products");
+const cartDiv = document.getElementById("cart");
+const totalDiv = document.getElementById("total");
+const saveBtn = document.getElementById("saveOrder");
+const tableSelect = document.getElementById("tableSelect");
+
+
+// Ürünleri yükle
+
+async function loadProducts(){
+
+    products = await getProducts();
+
+    productsDiv.innerHTML = "";
+
+    products.forEach(product => {
+
+        const div = document.createElement("div");
+
+        div.className = "product";
+
+        div.innerHTML = `
+            <h3>${product.name}</h3>
+            <p>${product.price} TL</p>
+        `;
+
+
+        div.onclick = () => addCart(product);
+
+        productsDiv.appendChild(div);
+
+    });
+
+}
+
+
+// Sepete ekle
+
+function addCart(product){
+
+    const exist = cart.find(item => item.id === product.id);
+
+
+    if(exist){
+
+        exist.qty++;
+
+    }else{
+
+        cart.push({
+            id: product.id,
+            name: product.name,
+            price: product.price,
+            qty: 1
+        });
+
+    }
+
+
+    renderCart();
+
+}
+
+
+// Sepeti göster
+
+function renderCart(){
+
+    cartDiv.innerHTML = "";
+
+    let total = 0;
+
+
+    cart.forEach(item => {
+
+        total += item.price * item.qty;
+
+
+        cartDiv.innerHTML += `
+        <div class="cart-item">
+        <span>${item.name} x${item.qty}</span>
+        <span>${item.price * item.qty} TL</span>
+        </div>
+        `;
+
+    });
+
+
+    totalDiv.innerText = total + " TL";
+
+}
+
+
+// Sipariş kaydet
+
+saveBtn.onclick = async () => {
+
+
+    const table = tableSelect.value;
+
+
+    if(!table){
+
+        alert("Masa seçiniz");
+        return;
+
+    }
+
+
+    if(cart.length === 0){
+
+        alert("Sepet boş");
+        return;
+
+    }
+
+
+    let total = cart.reduce(
+        (sum,item)=> sum + (item.price * item.qty),
+        0
+    );
+
+
+    await addDoc(collection(db,"orders"),{
+
+        table: table,
+
+        items: cart,
+
+        total: total,
+
+        status:"open",
+
+        createdAt: serverTimestamp()
+
+    });
+
+
+    alert("Sipariş kaydedildi");
+
+
+    cart = [];
+
+    renderCart();
+
+};
+
+
+
+loadProducts();
