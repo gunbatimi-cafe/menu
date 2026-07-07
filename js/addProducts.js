7b5e9e61f9854a0744d56b801aed8f2d68dddaaf
gunbatimi-cafe <kocdogukan98@gmail.com>
2026-07-07 17:00:44 +0000

    Create addProducts.js

---
+++ js/addProducts.js
@@ -0,0 +1,44 @@
+const products = [
+
+/* Kahve */
+{name:"Americano",price:160,category:"Kahve"},
+{name:"Latte",price:175,category:"Kahve"},
+{name:"Cappuccino",price:175,category:"Kahve"},
+{name:"Flat White",price:175,category:"Kahve"},
+{name:"Mocha",price:195,category:"Kahve"},
+{name:"Caramel Macchiato",price:195,category:"Kahve"},
+{name:"Türk Kahvesi",price:100,category:"Kahve"},
+{name:"Filtre Kahve",price:150,category:"Kahve"},
+{name:"Iced Filter Coffee",price:150,category:"Soğuk Kahve"},
+
+/* Çay */
+{name:"Siyah Çay",price:25,category:"Çay"},
+{name:"Ada Çayı",price:25,category:"Çay"},
+
+/* Tatlı */
+{name:"Waffle Tabak",price:300,category:"Tatlı"},
+{name:"Waffle Bardak",price:250,category:"Tatlı"},
+{name:"Dondurma 1 Top",price:60,category:"Tatlı"},
+
+/* Atıştırmalık */
+{name:"Nugget Tabağı",price:150,category:"Atıştırmalık"},
+{name:"Sosis Tabağı",price:150,category:"Atıştırmalık"},
+{name:"Patates Tabağı",price:150,category:"Atıştırmalık"},
+{name:"Karışık Tabak",price:300,category:"Atıştırmalık"},
+
+/* Burger */
+{name:"Chicken Burger",price:200,category:"Burger"},
+{name:"Chicken Burger Menü",price:300,category:"Burger"},
+
+/* Tost */
+{name:"Tost",price:150,category:"Tost"},
+{name:"Patsos",price:200,category:"Tost"},
+
+/* İçecek */
+{name:"Milkshake",price:250,category:"İçecek"},
+{name:"Kola",price:100,category:"İçecek"},
+{name:"Gazoz",price:100,category:"İçecek"},
+{name:"Soda",price:50,category:"İçecek"},
+{name:"Su",price:25,category:"İçecek"}
+
+];
