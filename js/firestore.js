aedd1628c38cab19188b37bb2e839381a44bc604
gunbatimi-cafe <kocdogukan98@gmail.com>
2026-07-07 23:58:07 +0300

    Enhance getProducts function structure and sorting
    Refactor getProducts function for improved readability and sorting.

--- js/firestore.js
+++ js/firestore.js
@@ -1,21 +1,39 @@
 import { db } from "./firebase.js";
+
 import {
-  collection,
-  getDocs
+    collection,
+    getDocs
 } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
 export async function getProducts() {
 
     const products = [];
 
-    const querySnapshot = await getDocs(collection(db, "products"));
+    const querySnapshot = await getDocs(
+        collection(db, "products")
+    );
 
     querySnapshot.forEach((doc) => {
 
         products.push({
+
             id: doc.id,
             ...doc.data()
+
         });
+
+    });
+
+    products.sort((a, b) => {
+
+        if (a.category === b.category) {
+            return a.sort - b.sort;
+        }
+
+        return a.category.localeCompare(
+            b.category,
+            "tr"
+        );
 
     });
 
